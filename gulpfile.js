910e15a7fa0c7d14adbe016f5c08b6534c50b96d
Raul Felipe de Melo <melo.raulf@gmail.com>
2016-11-15 21:00:22 -0200

    Adicionado dependencias para build e deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,37 @@
 const gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browser = require('browser-sync');
+    browser = require('browser-sync'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    ghPages = require('gulp-gh-pages'),
+    clean = require('gulp-clean');
+
+
+gulp.task('build',['clean'],function(){
+    gulp.start('sass','usemin','copyFiles');
+})
+
+gulp.task('usemin',function(){
+    return gulp.src('src/**/*.html')
+        .pipe(usemin({
+           // 'js': [uglify],
+            'css': [autoprefixer,cssmin]
+        }))
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('clean', function() {
+    return gulp.src('dist/')
+        .pipe(clean());
+});
+
+//Copia os outros arquivos arquivos
+gulp.task('copyFiles', function() {
+    gulp.src('src/img/**/*')
+        .pipe(gulp.dest('dist/img/'));
+});
 
 gulp.task('sass', function () {
 
@@ -14,11 +45,17 @@ gulp.task('server', function () {
 
     browser.init({
         server: {
-            baseDir: 'src/'
+            baseDir: 'dist/'
         }
     });
 
     //Change Listeners
     gulp.watch('src/sass/**/*.scss', ['sass']);
     gulp.watch('src/**/*.*').on('change', browser.reload);
-})
+});
+
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+        .pipe(ghPages());
+});
+

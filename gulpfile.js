1ec6239a8e7e26af9da9bbdd59e41000c8580cbb
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-06-30 15:49:32 +0100

    Added CSS build task and fixed watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,31 @@
+const autoprefixer = require('autoprefixer')
+const cssnano = require('gulp-cssnano')
 const gulp = require('gulp')
+const postcss = require('gulp-postcss')
+const postImport = require('postcss-import')
 const pug = require('gulp-pug')
+const rename = require('gulp-rename')
+const sourceMaps = require('gulp-sourcemaps')
 
-gulp.task('pug', function buildHTML() {
-  return gulp.src('src/index.pug')
-    .pipe(pug({}))
-    .pipe(gulp.dest('dist/'))
-});
-
-gulp.task('css',()=>{
-
+gulp.task('pug', () => {
+	return gulp.src('src/index.pug')
+		.pipe(pug({}))
+		.pipe(gulp.dest('dist/'))
 })
 
-gulp.task('watch', ()=>{
-  gulp.watch('src/**/*.pug', ['pug']);
-  gulp.watch('src/**/*.css').on('change', browser.reload);
+gulp.task('css', () => {
+	gulp.src('src/css/index.css')
+		.pipe(sourceMaps.init())
+		.pipe(postcss([autoprefixer(), postImport()]))
+		.pipe(cssnano())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(sourceMaps.write('.'))
+		.pipe(gulp.dest('dist/'))
+})
+
+gulp.task('watch', () => {
+	gulp.watch('src/**/*.pug', ['pug'])
+	gulp.watch('src/**/*.css', ['css'])
 })

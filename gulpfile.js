427508981a082f374d402ed2bc1a1b0126753f92
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-07-03 16:24:32 +0100

    Add JS task and dinamyc Data

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const autoprefixer = require('autoprefixer')
+const babel = require('gulp-babel')
+const concat = require('gulp-concat')
 const cssnano = require('gulp-cssnano')
+const data = require('gulp-data')
 const gulp = require('gulp')
+const jsmin = require('gulp-jsmin')
+const mergeJson = require('merge-json')
 const postcss = require('gulp-postcss')
 const postImport = require('postcss-import')
 const pug = require('gulp-pug')
@@ -9,6 +14,13 @@ const sourceMaps = require('gulp-sourcemaps')
 
 gulp.task('pug', () => {
 	return gulp.src('src/index.pug')
+		.pipe(data(() => {
+			let result = mergeJson.merge(require('./src/data/projects.json'),
+				require('./src/data/about.json'))
+			result = mergeJson.merge(result, require('./src/data/skills.json'))
+
+			return result
+		}))
 		.pipe(pug({}))
 		.pipe(gulp.dest('dist/'))
 })
@@ -25,7 +37,17 @@ gulp.task('css', () => {
 		.pipe(gulp.dest('dist/'))
 })
 
+gulp.task('js', () => {
+	return gulp.src(['./src/js/vendor/smooths-scroll.min.js', './src/js/index.js'])
+		.pipe(sourceMaps.init())
+		.pipe(babel())
+		.pipe(concat('index.min.js'))
+		.pipe(sourceMaps.write())
+		.pipe(gulp.dest('./dist/'))
+})
+
 gulp.task('watch', () => {
 	gulp.watch('src/**/*.pug', ['pug'])
 	gulp.watch('src/**/*.css', ['css'])
+	gulp.watch('src/**/*.js', ['js'])
 })

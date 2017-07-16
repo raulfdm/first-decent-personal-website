0ee7c6a76e8e5214671eea462b42e4dbcd900cfe
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-07-16 21:07:22 +0100

    Code formatted

--- src/js/index.js
+++ src/js/index.js
@@ -1,68 +1,66 @@
-$(document).ready(function () {
+$( document ).ready( function () {
+  const $window = $( window )
+  const $menuItems = $( '.header__menu__list__item__link' )
 
-	const $window = $(window)
-	const $menuItems = $('.header__menu__list__item__link')
+  let scrollPosition = 0
 
-	let scrollPosition = 0
+  const handleScroll = () => {
+    const actualScrollPosition = $window.scrollTop()
 
-	const handleScroll = () => {
-		const actualScrollPosition = $window.scrollTop()
+    actualScrollPosition === 0 ?
+      handleHeaderStates.transparencyOn() :
+      handleHeaderStates.transparencyOff()
 
-		actualScrollPosition === 0 ?
-			handleHeaderStates.transparencyOn() :
-			handleHeaderStates.transparencyOff()
+    actualScrollPosition < scrollPosition ?
+      handleHeaderStates.show() :
+      handleHeaderStates.hide()
 
-		actualScrollPosition < scrollPosition ?
-			handleHeaderStates.show() :
-			handleHeaderStates.hide()
+    scrollPosition = actualScrollPosition
+  }
 
-		scrollPosition = actualScrollPosition;
-	}
+  const handleHeaderStates = {
+    $header: $( '.header' ),
+    hide() {
+      this.$header.addClass( 'hidden' )
+    },
+    show() {
+      this.$header.removeClass( 'hidden' )
+      return this
+    },
+    transparencyOn() {
+      this.$header.addClass( 'is-transparent' )
+      return this
+    },
+    transparencyOff() {
+      this.$header.removeClass( 'is-transparent' )
+      return this
+    }
+  }
 
-	const handleHeaderStates = {
-		$header: $('.header'),
-		hide() {
-			this.$header.addClass('hidden')
-		},
-		show() {
-			this.$header.removeClass('hidden')
-			return this
-		},
-		transparencyOn() {
-			this.$header.addClass('is-transparent')
-			return this
-		},
-		transparencyOff() {
-			this.$header.removeClass('is-transparent')
-			return this
-		}
-	}
+  const handleURL = () => history.replaceState( {}, null, '/' )
 
-	const handleURL = () => history.replaceState({}, null, '/')
+  /******** Confs ********/
 
-	/******** Confs ********/
+  $( '.home__title' ).typeIt( {
+    strings: [ 'Front-end Developer', 'Curious Guy',
+      'Passionate About Coding'
+    ],
+    breakLines: false,
+    loop: true,
+    deleteSpeed: 50,
+    deleteDelay: 2000,
+    loopDelay: 2000
+  } )
 
-	$('.home__title').typeIt({
-		strings: ['Front-end Developer', 'Curious Guy',
-			'Passionate About Coding'
-		],
-		breakLines: false,
-		loop: true,
-		deleteSpeed: 50,
-		deleteDelay: 2000,
-		loopDelay: 2000
-	})
+  $( 'html' ).smoothScroll( 400 )
 
-	$('html').smoothScroll(400)
+  $menuItems.click( () => {
+    setTimeout( function () {
+      handleHeaderStates.hide()
+    }, 500 )
+  } )
 
-
-	$menuItems.click(() => {
-		setTimeout(function () {
-			handleHeaderStates.hide()
-		}, 500);
-	})
-
-	$window.scroll(handleScroll)
-	/* Do not modify URL */
-	$window.on('hashchange', handleURL)
-})
+  $window.scroll( handleScroll )
+  /* Do not modify URL */
+  $window.on( 'hashchange', handleURL )
+} )

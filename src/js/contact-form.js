9967657af2e8c1bee6882b8d1e8181266a316f64
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-07-20 00:51:59 +0100

    Issue #24 - Refactored some functions and started to implements reCaptcha function

--- src/js/contact-form.js
+++ src/js/contact-form.js
@@ -1,29 +1,50 @@
 ;
 (() => {
 	emailjs.init("user_XDedmoeMTu9Eyl0FxOlTJ")
-	const $buttonSubmit = $('.js-form__submit')
 
-	const sendEmail = () => {}
-
-	const callReCaptcha = event => {
-		//grecaptcha.execute();
-		getFormValues()
+	const sendEmail = () => {
+		return new Promise((resolve, reject) => {
+			let a = 10
+			if (a == 10) {
+				resolve(true)
+			} else {
+				reject(false)
+			}
+		})
 	}
 
-	const getFormValues = () => {
+	const callReCaptcha = () => {
+		grecaptcha.execute()
+	}
+
+	const handleContactForm = () => {
 		let error = ""
 
-		const data = {
-			name: $('.js-form__name').val(),
-			subject: $('.js-form__subject').val(),
-			message: $('.js-form__message').val(),
-			mailFrom: $('.js-form__email').val()
-		}
+		const data = handleFormState.getData()
 
-		hasErrorsInForm(data)
-			.then(s => handleFeedbackMessage(true))
-			.catch(e => handleFeedbackMessage(false,e))
+		new Promise((resolve, reject) => {
+			hasErrorsInForm(data)
+				.then(data => {
+					callReCaptcha()
+					lockForm()
+					sendEmail(data)
+				})
+				.then(success => resolve(handleFeedbackMessage.success()))
+				.then(() => handleFormState.clearData())
+				.catch(e => handleFeedbackMessage.failure(e))
+				.then(() => unlockForm())
+		})
 
+	}
+
+	const lockForm = () => {
+		handleButtonState.disable()
+		handleFormState.disable()
+	}
+
+	const unlockForm = () => {
+		handleButtonState.enable()
+		handleFormState.enable()
 	}
 
 	const hasErrorsInForm = data => {
@@ -40,46 +61,69 @@
 			else if (!data.message)
 				invalidProp = "Message"
 
-			invalidProp ? reject(invalidProp + errorMessage) : resolve(true)
+			invalidProp ? reject(invalidProp + errorMessage) : resolve(data)
 
 		})
 	}
 
-	const handleFeedbackMessage = function(isPostive, errorMessage = "") {
-		if(!isPostive && (arguments.length !== 2 || errorMessage === ""))
-			throw new Error('Number of Arguments Invalid or error message not passed! Please, if you send "false", you must need to send second argument(error message)')
-
-		const $feedbackElement = $('.contact__form__feedback-message')
-
-
-		if (isPostive) {
-			$feedbackElement.text('Message sent successfuly!').addClass('is-success')
-		}else{
-			$feedbackElement.text('*'+errorMessage).addClass('is-failure')
+	const handleFeedbackMessage = {
+		$feedbackElement: $('.contact__form__feedback-message'),
+		failure(errorMessage = "") {
+			if (!errorMessage)
+				throw new Error('You must inform an error message')
+			this.$feedbackElement.text('*' + errorMessage).addClass('is-failure')
+		},
+		success() {
+			this.$feedbackElement.text('Message sent successfuly!').addClass(
+				'is-success')
+		},
+		clean() {
+			this.$feedbackElement.removeClass('is-failure').removeClass('is-success')
 		}
 	}
 
-	const formControl = {
+	const handleFormState = {
 		fields: {
 			name: $('.js-form__name'),
 			subject: $('.js-form__subject'),
 			message: $('.js-form__message'),
 			mailFrom: $('.js-form__email')
 		},
-		getData(){
-			return Object.getOwnPropertyNames(this.fields).reduce((result, actual) => {
-				console.log(result,actual)
-				return result[actual] = this.fields[actual]
-			},{})
+		getData() {
+			return Object.getOwnPropertyNames(this.fields).reduce((result, fieldKey) => {
+				result[fieldKey] = this.fields[fieldKey].val()
+				return result
+			}, {})
+		},
+		disable() {
+			Object.getOwnPropertyNames(this.fields).map(fieldKey => this.fields[
+				fieldKey].attr('disabled', true).addClass('is-disabled'))
+		},
+		enable() {
+			Object.getOwnPropertyNames(this.fields).map(fieldKey => this.fields[
+				fieldKey].attr('disabled', false).removeClass('is-disabled'))
+		},
+		clearData() {
+			Object.getOwnPropertyNames(this.fields).map(fieldKey => this.fields[
+				fieldKey].val(''))
 		}
 	}
 
-	const handleButtonState = () => {
-
+	const handleButtonState = {
+		$buttonElement: $('.js-form__submit'),
+		clickEvent(action) {
+			this.$buttonElement.on('click', action)
+		},
+		disable() {
+			this.$buttonElement.addClass('is-disabled').attr('disabled', true)
+		},
+		enable() {
+			this.$buttonElement.removeClass('is-disabled').attr('disabled', false)
+		}
 	}
 
-	$buttonSubmit.on('click', callReCaptcha)
+	handleButtonState.clickEvent(handleContactForm)
+
 	$('.contact__form').on('submit', e => e.preventDefault())
 
-	console.log(formControl.getData())
 })()

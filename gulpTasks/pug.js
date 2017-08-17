36d55dcbf95f29f1db6e191ffda3dbbc55df332a
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-08-17 11:22:36 +0100

    Retrieving data from an external API (Firebase) instead json files

--- gulpTasks/pug.js
+++ gulpTasks/pug.js
@@ -2,23 +2,28 @@ const gulp = require('gulp')
 const data = require('gulp-data')
 const pug = require('gulp-pug')
 const path = require('path')
+const fetch = require('node-fetch')
 
 module.exports = options => {
   gulp.task('pug', () => {
     return gulp
       .src('src/index.pug')
       .pipe(
-        data(() => {
-          const result = {
-            projects: require('../src/data/projects.json'),
-            about: require('../src/data/about.json'),
-            abilities: require('../src/data/skills.json'),
+        data(async () => {
+          try {
+            const response = await fetch(
+              'https://personal-website-api.firebaseio.com/infos.json',
+              {},
+            )
+            const data = await response.json()
+
+            if (options.production)
+              data.assets = require('../src/data/rev-manifest.json')
+
+            return data
+          } catch (error) {
+            console.error(error)
           }
-
-          if (options.production)
-            result.assets = require('../src/data/rev-manifest.json')
-
-          return result
         }),
       )
       .pipe(pug({}))
